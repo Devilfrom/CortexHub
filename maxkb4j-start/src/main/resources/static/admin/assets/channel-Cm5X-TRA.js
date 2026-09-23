55285b194901edec07316a776ffcc83f2be98cc3
Devilfrom <Devilfrom@users.noreply.github.com>
2026-09-23 15:41:15 +0800

    rebrand import batch 30/40

---
+++ maxkb4j-start/src/main/resources/static/admin/assets/channel-Cm5X-TRA.js
@@ -0,0 +1,4 @@
+import {ar as o, as as s} from "./admin-Clc14HGN.js";
+
+const t = (r, a) => o.lang.round(s.parse(r)[a]);
+export {t as c};
---
+++ maxkb4j-start/src/main/resources/static/admin/assets/clone-CP9fx6np.js
@@ -0,0 +1,9 @@
+import {aC as r} from "./admin-Clc14HGN.js";
+
+var a = 4;
+
+function n(o) {
+    return r(o, a)
+}
+
+export {n as c};
